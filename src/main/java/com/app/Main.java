d7ea7d27779daadf9e92340fdc710e1150dd28df
Fernando Staroski <fernandostaroski@gmail.com>
2018-06-02 15:21:23 -0300

    ajuste para deploy em wildfly

--- src/main/java/com/app/Main.java
+++ src/main/java/com/app/Main.java
@@ -2,14 +2,22 @@ package com.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.web.SpringServletContainerInitializer;
 
 /**
  * Created by Fernando on 31/05/2018.
  */
 @SpringBootApplication
-public class Main {
+public class Main extends SpringBootServletInitializer {
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Main.class,args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(getClass());
     }
 }
